aad76ffb6511deefc994203aed196d9896e498e1
moni fatecha <fbmonib@gmail.com>
2019-09-23 18:47:26 -0400

    delete some lines

--- app/src/main/java/smarttraffic/smartmoving/SMovingInitialToken.java
+++ app/src/main/java/smarttraffic/smartmoving/SMovingInitialToken.java
@@ -2,6 +2,6 @@ package smarttraffic.smartmoving;
 
 public class SMovingInitialToken {
     private static final String token =
-            "b48186acfaad65fb47e9148794cdd3969cb3cf7f";
+            "";
     public static String getToken(){ return token;}
 }
