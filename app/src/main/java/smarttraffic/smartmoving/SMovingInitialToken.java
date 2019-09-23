c01b472a2e86293e9957c65a4ab68617e3b0ddd3
moni fatecha <fbmonib@gmail.com>
2019-09-23 18:49:29 -0400

    delete file

--- app/src/main/java/smarttraffic/smartmoving/SMovingInitialToken.java
+++
@@ -1,7 +0,0 @@
-package smarttraffic.smartmoving;
-
-public class SMovingInitialToken {
-    private static final String token =
-            "";
-    public static String getToken(){ return token;}
-}
